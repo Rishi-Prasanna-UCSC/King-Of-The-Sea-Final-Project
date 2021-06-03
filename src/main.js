3c5be90f23de2b14ed3a8887717497f00384b779
“Rishi <riprasan@ucsc.edu>
2021-06-03 10:21:53 -0700

    Non-debug mode (for playtesting Level One)

--- src/main.js
+++ src/main.js
@@ -7,13 +7,13 @@ let config = {
     type: Phaser.CANVAS,
     width: widthScreen,
     height: heightScreen,
-    // scene: [Intro, Menu, Levels, One, Pause, Tutorial, LevelComplete, GameOver, Two],
-    scene: [One, Pause, LevelComplete, GameOver, Two], // Debug
+    scene: [Intro, Menu, Levels, One, Pause, Tutorial, LevelComplete, GameOver, Two],
+    // scene: [One, Pause, LevelComplete, GameOver, Two], // Debug
     physics: {
         default: 'arcade',
         arcade: {
-            // debug: false
-            debug: true
+            debug: false
+            // debug: true
         }
     }
 }
