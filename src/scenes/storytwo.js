8fc4f545a2aea498cde81f64f03d35ae1a0f6980
“Rishi <riprasan@ucsc.edu>
2021-06-07 23:02:12 -0700

    Changed dialogue slightly

--- src/scenes/storytwo.js
+++ src/scenes/storytwo.js
@@ -11,7 +11,7 @@ class StoryTwo extends Phaser.Scene {
         this.cameras.main.fadeIn(2000, 0, 0, 0);
         this.add.text(250, 100, "I was once a great white shark."); 
         this.add.text(120, 140, "You know the ones I'm talking about.\nBlimey, they made a movie about me?");
-        this.add.text(90, 260, "I savvy that fame tempted me to bite off more than I could chew.");
+        this.add.text(90, 260, "I savvy that attitude tempted me to bite off more than I could chew.");
         this.add.text(130, 300, "Anywho, ye, I remember now. I saw this big flash of light...");
 
         this.input.on('pointerdown', () => {
